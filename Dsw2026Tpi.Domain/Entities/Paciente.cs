1f26977702a585ab716f448acdf99dea07d92d90
MiltonTolaba <Milton.Tolaba@alu.frt.utn.edu.ar>
2026-07-20 20:04:17 -0300

    revision login de administrador y paciente

--- Dsw2026Tpi.Domain/Entities/Paciente.cs
+++ Dsw2026Tpi.Domain/Entities/Paciente.cs
@@ -7,15 +7,17 @@ namespace Dsw2026Tpi.Domain.Entities
     public class Paciente : EntityBase
     {
         public int Dni { get; set; }
+        public string Email { get; set; }
         public string Name { get; set; }
         public string Cellnumber { get; set; }
         private Paciente()
         {
             
         }
-        public Paciente(int dni, string name, string cellnumber, Guid? id = null) : base(id)
+        public Paciente(int dni, string email, string name = "", string cellnumber = "", Guid? id = null) : base(id)
         { 
             Dni = dni;
+            Email = email;
             Name = name;
             Cellnumber = cellnumber;
         }
