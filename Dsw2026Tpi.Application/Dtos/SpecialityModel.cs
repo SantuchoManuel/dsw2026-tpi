f875512cfd98b348e1a5cc29c54168da126acaf2
joel <angeljoeltoledo@gmail.com>
2026-07-20 20:03:55 -0300

    Agregacion de EndPoints

---
+++ Dsw2026Tpi.Application/Dtos/SpecialityModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Application.Dtos;
+
+public record SpecialityModel
+{      
+    public record Request(string Name, string Description);
+    public record Response(Guid Id, string Name, string Description);
+}
+
+/*
+
+Especialidad
+Response
+{
+"pageSize": 0,
+"pageIndex": 0,
+"data": [
+        {
+        "id": "Guid",
+        "name": "string",
+        "description”: "string"
+        },
+        ],
+"total": number
+}
+
+
+ */
