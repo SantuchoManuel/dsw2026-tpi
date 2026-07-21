6a0ec11e1328ae4f282c025d347e81b565d8f2ff
SantuchoManuel <manusan2804@gmail.com>
2026-07-20 21:27:05 -0300

    Cita Endpoints hechos

---
+++ Dsw2026Tpi.Api/Controllers/CitaController.cs
@@ -0,0 +1,67 @@
+﻿using Dsw2026Tpi.Application.Dtos;
+using Dsw2026Tpi.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2026Tpi.Api.Controllers
+{
+    [Route("appointments")]
+    public class CitaController : AppController
+    {
+        private readonly ICitaService _service;
+
+        public CitaController(ICitaService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SolicitarTurno([FromBody] CitaModel.Request request)
+        {
+            if (request.DoctorId == Guid.Empty)
+            {
+                return BadRequest("El DoctorId es obligatorio.");
+            }
+            if (request.AvailabilityId == Guid.Empty)
+            {
+                return BadRequest("El AvailabilityId es obligatorio.");
+            }
+
+            var dniString = request.Patient?.Dni.ToString();
+            if (string.IsNullOrEmpty(dniString) || dniString.Length < 7 || dniString.Length > 10)
+            {
+                return BadRequest("DNI obligatorio y debe tener entre 7 y 10 dígitos.");
+            }
+                
+            if (string.IsNullOrWhiteSpace(request.Reason) || request.Reason.Length < 5)
+            {
+                return BadRequest("El motivo (reason) es obligatorio y debe tener al menos 5 caracteres.");
+            }
+            
+            await _service.CrearCitaAsync(request);
+
+            return Ok();
+        }
+
+        [HttpGet("patient?dni={dni}")]
+        public async Task<IActionResult> VerTurnosPaciente([FromQuery] int dni)
+        {
+            if (dni <= 0)
+            {
+                return BadRequest("DNI inválido.");
+            } 
+            var turnos = await _service.ObtenerTurnosPacienteAsync(dni);
+            return Ok(turnos);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> CancelarTurno(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return BadRequest("ID de cita inválido.");
+            }
+            await _service.CancelarCitaAsync(id);
+            return Ok();
+        }
+    }
+}
---
+++ Dsw2026Tpi.Application/Interfaces/ICitaService.cs
@@ -0,0 +1,14 @@
+﻿using Dsw2026Tpi.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Application.Interfaces
+{
+    public interface ICitaService
+    {
+        Task CrearCitaAsync(CitaModel.Request peticion);
+        Task<List<CitaModel.Response>> ObtenerTurnosPacienteAsync(int dni);
+        Task CancelarCitaAsync(Guid citaId);
+    }
+}
