ce09e9ff3733de27dd19b96f8d7e24e28ed22310
joel <angeljoeltoledo@gmail.com>
2026-07-24 18:56:24 -0300

    SpecialityValidations pasadas al Service

--- Dsw2026Tpi.Api/Controllers/SpecialityController.cs
+++ Dsw2026Tpi.Api/Controllers/SpecialityController.cs
@@ -1,12 +1,8 @@
 using Dsw2026Tpi.Application.Dtos;
 using Dsw2026Tpi.Application.Interfaces;
-using Dsw2026Tpi.Application.Services;
-using Dsw2026Tpi.CrossCutting.Exceptions;
 using Dsw2026Tpi.CrossCutting.Identity;
-using Dsw2026Tpi.CrossCutting.Resources;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Dsw2026Tpi.Api.Controllers;
 
@@ -24,14 +20,6 @@ public class SpecialityController : AppController
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<IActionResult> GetAll([FromQuery] int pageSize = 10, [FromQuery] int pageIndex = 1, [FromQuery] string? name = null)
     {
-        if (!string.IsNullOrEmpty(name) && (name.Length < 3 || name.Length > 100))
-        {
-            throw new ValidationException(
-                string.Format(ErrorCodes.FIELD_LENGTH_INVALID, "name", 3, 100),
-                nameof(ErrorCodes.FIELD_LENGTH_INVALID)
-            ).WithDetail("name", "La longitud es inválida");
-        }
-        
         var speciality = await _service.GetAll(pageSize, pageIndex, name);
         return Ok(speciality);
     }
@@ -41,78 +29,15 @@ public class SpecialityController : AppController
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Create([FromBody] SpecialityModel.Request request)
     {
-        if (string.IsNullOrWhiteSpace(request.Name))
-        {
-            throw new ValidationException(
-                string.Format(ErrorCodes.FIELD_REQUIRED, "name"),
-                nameof(ErrorCodes.FIELD_REQUIRED)
-            ).WithDetail("name", "Es requerido");
-        }
-
-        if (request.Name.Length < 3 || request.Name.Length > 100)
-        {
-            throw new ValidationException(
-                string.Format(ErrorCodes.FIELD_LENGTH_INVALID, "name", 3, 100),
-                nameof(ErrorCodes.FIELD_LENGTH_INVALID)
-            ).WithDetail("name", "La longitud es inválida");
-        }
-
-        if (string.IsNullOrWhiteSpace(request.Description))
-        {
-            throw new ValidationException(
-                string.Format(ErrorCodes.FIELD_REQUIRED, "description"),
-                nameof(ErrorCodes.FIELD_REQUIRED)
-            ).WithDetail("description", "Es requerido");
-        }
-
-        if (request.Description.Length < 10 || request.Description.Length > 100)
-        {
-            throw new ValidationException(
-                string.Format(ErrorCodes.FIELD_LENGTH_INVALID, "description", 10, 100),
-                nameof(ErrorCodes.FIELD_LENGTH_INVALID)
-            ).WithDetail("description", "La longitud es inválida");
-        }
-
         var speciality = await _service.Add(request);
         return Ok(speciality);
     }
+
     [HttpPut("specialities/{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Update(Guid id, [FromBody] SpecialityModel.Request request)
     {
-        if (string.IsNullOrWhiteSpace(request.Name))
-        {
-            throw new ValidationException(
-                string.Format(ErrorCodes.FIELD_REQUIRED, "name"),
-                nameof(ErrorCodes.FIELD_REQUIRED)
-            ).WithDetail("name", "Es requerido");
-        }
-
-        if (request.Name.Length < 3 || request.Name.Length > 100)
-        {
-            throw new ValidationException(
-                string.Format(ErrorCodes.FIELD_LENGTH_INVALID, "name", 3, 100),
-                nameof(ErrorCodes.FIELD_LENGTH_INVALID)
-            ).WithDetail("name", "La longitud es inválida");
-        }
-
-        if (string.IsNullOrWhiteSpace(request.Description))
-        {
-            throw new ValidationException(
-                string.Format(ErrorCodes.FIELD_REQUIRED, "description"),
-                nameof(ErrorCodes.FIELD_REQUIRED)
-            ).WithDetail("description", "Es requerido");
-        }
-
-        if (request.Description.Length < 10 || request.Description.Length > 100)
-        {
-            throw new ValidationException(
-                string.Format(ErrorCodes.FIELD_LENGTH_INVALID, "description", 10, 100),
-                nameof(ErrorCodes.FIELD_LENGTH_INVALID)
-            ).WithDetail("description", "La longitud es inválida");
-        }
-
         var speciality = await _service.Update(id, request);
         return Ok(speciality);
     }
