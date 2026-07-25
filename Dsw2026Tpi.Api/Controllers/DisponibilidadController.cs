694990ab60b90506ef58b49ff5c0de79e222d51d
joel <angeljoeltoledo@gmail.com>
2026-07-24 22:29:40 -0300

    Validaciones en Service y Aplicacion de Codigo de Error

--- Dsw2026Tpi.Api/Controllers/DisponibilidadController.cs
+++ Dsw2026Tpi.Api/Controllers/DisponibilidadController.cs
@@ -1,8 +1,10 @@
 ﻿using Dsw2026Tpi.Application.Dtos;
 using Dsw2026Tpi.Application.Interfaces;
+using Dsw2026Tpi.CrossCutting.Exceptions;
 using Dsw2026Tpi.CrossCutting.Identity;
-using Microsoft.AspNetCore.Mvc;
+using Dsw2026Tpi.CrossCutting.Resources;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dsw2026Tpi.Api.Controllers
 {
@@ -22,10 +24,19 @@ namespace Dsw2026Tpi.Api.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Create([FromBody] DisponibilidadModel.Request request)
         {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
+            // cuando ponemos "" en doctorid tira nohadle error, esto es porque el guid no puede ser vacio, pero si es null o vacio tira error de validacion
             await _service.CrearDisponibilidadAsync(request);
             return Ok();
+            /*
+                    Esto no esta implementado Importante Verlo 
+
+            ● El backend debe crear en la base de datos un registro por cada intervalo de
+            30min dentro del rango de startTime y endTime.
+            ● Ejemplo: si se indica que el doctor atiende Lunes desde las 9am hasta las 12pm,
+            el sistema debe registrar para cada Lunes del mes turnos de 30 minutos entre
+            las 9am y 12pm.
+             */
+
         }
 
         [HttpPut]
@@ -33,8 +44,6 @@ namespace Dsw2026Tpi.Api.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Update([FromBody] DisponibilidadModel.Request request)
         {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
             await _service.ActualizarDisponibilidadAsync(request);
             return Ok();
         }
--- Dsw2026Tpi.Application/Dtos/DisponibilidadModel.cs
+++ Dsw2026Tpi.Application/Dtos/DisponibilidadModel.cs
@@ -7,23 +7,30 @@ namespace Dsw2026Tpi.Application.Dtos
 {
     public record DisponibilidadModel
     {
-        public record Request(
+        public record Request(Guid DoctorId, List<EsquemaDia> Days);
+        public record EsquemaDia( string Day,string StartTime, string EndTime);
+    }
+
+    /*
+  public record Request(
                     [Required(ErrorMessage = "El DoctorId es obligatorio.")]
             Guid DoctorId,
                     [Required(ErrorMessage = "Debe enviar al menos un día.")]
-            [MinLength(1, ErrorMessage = "Debe enviar al menos un día.")]
+                    [MinLength(1, ErrorMessage = "Debe enviar al menos un día.")]
             List<EsquemaDia> Days
                 );
-        public record EsquemaDia(
-            [Required(ErrorMessage = "El día es obligatorio.")]
-            [StringLength(20, ErrorMessage = "El día no puede superar 20 caracteres.")]
+
+    public record EsquemaDia(
+                [Required(ErrorMessage = "El día es obligatorio.")]
+                [StringLength(20, ErrorMessage = "El día no puede superar 20 caracteres.")]
             string Day,
-            [Required(ErrorMessage = "La hora de inicio es obligatoria.")]
-            [RegularExpression(@"^\d{2}:\d{2}$", ErrorMessage = "StartTime debe tener formato HH:mm.")]
+                [Required(ErrorMessage = "La hora de inicio es obligatoria.")]
+                [RegularExpression(@"^\d{2}:\d{2}$", ErrorMessage = "StartTime debe tener formato HH:mm.")]
             string StartTime,
-            [Required(ErrorMessage = "La hora de fin es obligatoria.")]
-            [RegularExpression(@"^\d{2}:\d{2}$", ErrorMessage = "EndTime debe tener formato HH:mm.")]
+                [Required(ErrorMessage = "La hora de fin es obligatoria.")]
+                [RegularExpression(@"^\d{2}:\d{2}$", ErrorMessage = "EndTime debe tener formato HH:mm.")]
             string EndTime
         );
-    }
+
+  */
 }
