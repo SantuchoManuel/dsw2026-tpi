8bc51bb7cc878cf03e5c72afa9023b886f754dc6
SantuchoManuel <manusan2804@gmail.com>
2026-07-29 17:05:36 -0300

    Añadido Id de Turnos en Response de Doctor

--- Dsw2026Tpi.Application/Services/DoctorService.cs
+++ Dsw2026Tpi.Application/Services/DoctorService.cs
@@ -88,13 +88,21 @@ public class DoctorService : IDoctorService
         var doctor = await _persistence.First<Doctor>(d => d.Id == id && d.IsActive);
         if (doctor == null) throw new EntityNotFoundException("Médico").WithDetail(" Medico", "No Encontrado");
 
-        var availabilities = await _persistence.GetFiltered<Disponibilidad>(a => a.DoctorId == id);
+        var turnos = await _persistence.GetFiltered<Turno>(
+            t => t.Disponibilidad.DoctorId == id && (int)t.EstadoTurno == 0,
+            "Disponibilidad" 
+        );
 
-        return availabilities?.Select(a => new DoctorModel.AvailabilityResponse(
-            a.DiaDeLaSemana.ToString(),
-            a.HoraDeEntrada.ToString("HH:mm"),
-            a.HoraDeSalida.ToString("HH:mm")
-        )).ToList() ?? new List<DoctorModel.AvailabilityResponse>();
+        return turnos?.Select(t => new DoctorModel.AvailabilityResponse(
+            t.Id, 
+            t.Fecha.ToString("yyyy-MM-dd"), 
+            t.Fecha.DayOfWeek.ToString(),
+            t.HoraDeInicio.ToString("HH:mm"),
+            t.HoraDeFin.ToString("HH:mm")
+        ))
+        .OrderBy(t => t.Date)
+        .ThenBy(t => t.StartTime)
+        .ToList() ?? new List<DoctorModel.AvailabilityResponse>();
     }
 
     private async Task<DoctorModel.Response> GetByIdInternal(Guid id)
