4fa8c0957b5e6a12fad9edf1d87c80503ac48bd2
SantuchoManuel <manusan2804@gmail.com>
2026-08-02 22:52:08 -0300

    Disponibilidad Recuperado

---
+++ Dsw2026Tpi.Api/Controllers/DisponibilidadController.cs
@@ -0,0 +1,40 @@
+﻿using Dsw2026Tpi.Application.Dtos;
+using Dsw2026Tpi.Application.Interfaces;
+using Dsw2026Tpi.CrossCutting.Exceptions;
+using Dsw2026Tpi.CrossCutting.Identity;
+using Dsw2026Tpi.CrossCutting.Resources;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2026Tpi.Api.Controllers
+{
+    [Authorize(Policy = Policies.AdminPolicy)]
+    [Route("availabilities")]
+    public class DisponibilidadController : AppController
+    {
+        private readonly IDisponibilidadService _service;
+
+        public DisponibilidadController(IDisponibilidadService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Create([FromBody] DisponibilidadModel.Request request)
+        {
+            await _service.CrearDisponibilidadAsync(request);
+            return Ok();
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update([FromBody] DisponibilidadModel.Request request)
+        {
+            await _service.ActualizarDisponibilidadAsync(request);
+            return Ok();
+        }
+    }
+}
